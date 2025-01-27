87375e2e680a540810eed33403f0405a232b67d7
Luuke23 <tomo.zawa.97.1.11@gmail.com>
2025-01-27 23:02:49 +0900

    add admin-home

---
+++ src/main/java/com/example/nagoyameshi/controller/AdminHomeController.java
@@ -0,0 +1,13 @@
+package com.example.nagoyameshi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminHomeController {
+	@GetMapping("/admin/home")
+	public String index(Model model) {
+		return "admin/index";
+	}
+}
